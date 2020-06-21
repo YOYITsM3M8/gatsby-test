6a98163d69b447e8d9366c2fa1eb9ad902e1c50b
YOYITsM3M8 <49005044+YOYITsM3M8@users.noreply.github.com>
2020-06-21 08:10:26 -0400

    Update date.ts

--- api/date.ts
+++ api/date.ts
@@ -1,6 +1,6 @@
 import { NowRequest, NowResponse } from '@now/node';
 
 export default (_req: NowRequest, res: NowResponse) => {
-  const date = new Date().toString();
+  const date = "what is th date?"
   res.status(200).send(date);
 };
