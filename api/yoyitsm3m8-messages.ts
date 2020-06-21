fac8eddcbc055d952b840fbf1c8dc7b67e36f90d
YOYITsM3M8 <49005044+YOYITsM3M8@users.noreply.github.com>
2020-06-21 17:20:56 -0400

    Update yoyitsm3m8-messages.ts

--- api/yoyitsm3m8-messages.ts
+++ api/yoyitsm3m8-messages.ts
@@ -1,20 +1,6 @@
-import { NowRequest, NowResponse } from '@now/node';
+import { NowRequest, NowResponse } from '@vercel/node'
 
-export default (_req: NowRequest, res: NowResponse) => {
-  const date = "what is th date?"
-  res.status(200).send(date);
-};
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       oof = xhttp.responseText;
-    }
-};
-xhttp.open("GET", "https://api.scratch.mit.edu/users/yoyitsm3m8/count", true);
-xhttp.send();
-
-export default (_req: NowRequest, res: NowResponse) => {
-  res.status(200).send(oof);
-};
+export default (request: NowRequest, response: NowResponse) => {
+  const { name = 'World' } = request.query
+  response.status(200).send(`Hello ${name}!`)
+}
