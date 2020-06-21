e662eb6cbc16ab7b8326431b194a22ffac68688e
YOYITsM3M8 <49005044+YOYITsM3M8@users.noreply.github.com>
2020-06-21 17:37:43 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ function Index() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/yoyitsm3m8-messages');
+      const res = await fetch('/api/date');
       const newDate = await res.text();
       setDate(newDate);
     }
@@ -48,7 +48,7 @@ function Index() {
       </p>
       <br />
       <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <p>{date ? date : '...'}</p>
     </main>
   );
 }
